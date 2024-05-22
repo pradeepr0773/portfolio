4c5e638f4da38d25782df5bd06869c5499d087b8
pradeepr0773 <139031178+pradeepr0773@users.noreply.github.com>
2024-05-22 10:43:30 +0530

    Update About.js

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -4,7 +4,7 @@ export default function About(){
             <h1 className="text-4xl border-b-4 mb-5 w-[170px] font-bold  m-5">About Me</h1>
             <div className="projectList m-5 p-5">
             <p className="m-5  pb-5 text-2xl">Enthusiastic fresher Full Stack Developer proficient in PHP, MySQL, MongoDB, Express, React, Node.js, and the MERN stack. Eager to apply skills in building dynamic web applications and contribute to innovative projects. Dedicated to delivering high-quality code and continuously learning.</p>
-            <p className="m-5  pb- text-2xl">This website is built using React.js and Tailwind CSS.</p>
+            <p className="m-5  pb- text-2xl">This website is built using MERN (MongoDB, Express.js, React.js, Node.Js) Stack and Tailwind CSS.</p>
         </div>
         </div>
         </section>
